e28eb1fd9fbb899b3c26bde7952ee098a87519aa
Jacob Adlers <jacob.adlers@gmail.com>
2018-08-05 00:21:56 +0200

    Welcome email if no displayName

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 const HomePage = ({ logoutUser, firebase }) => {
   const user = firebase.auth().currentUser;
+  let welcomeName = '';
+  if (user && user.displayName) {
+    welcomeName = user.displayName;
+  } else if (user && user.email) {
+    welcomeName = user.email.split('@')[0];
+  }
 
   return (
     <div>
       <h1>Home</h1>
       <button onClick={logoutUser}>Log out</button>
-      <p>Welcome {user && user.displayName}!</p>
+      <p>Welcome {welcomeName}</p>
     </div>
   );
 };
