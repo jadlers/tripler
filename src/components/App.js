327163cd3026a8851f73aca82ae1f7d61403019d
Jacob Adlers <jacob.adlers@gmail.com>
2018-08-04 02:27:16 +0200

    Create Landing, Home, and Login pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Navigation from './Navigation';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import HomePage from './HomePage.js';
+import LoginPage from './LoginPage.js';
+import Navigation from './Navigation.js';
+import LandingPage from './LandingPage.js';
+
+import * as routes from '../constants/routes';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Navigation />
+        <div>
+          <Navigation />
+          <hr />
+
+          <Route
+            exact
+            path={routes.LANDING}
+            component={() => <LandingPage />}
+          />
+          <Route exact path={routes.HOME} component={() => <HomePage />} />
+          <Route exact path={routes.LOGIN} component={() => <LoginPage />} />
+        </div>
       </Router>
     );
   }
