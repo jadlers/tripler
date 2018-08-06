04d4367ce67083f171b650d5f2d62c23abfef300
Jacob Adlers <jacob.adlers@gmail.com>
2018-08-06 19:26:33 +0200

    Add styled components & style Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const TopBar = styled.div`
+  display: flex;
+  padding: 0.5em 1em;
+  background: #00bcd4;
+  box-shadow: 0 1px 4px 1px;
+  margin: 0;
+  border-radius: 0 0 0.5em 0.5em;
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  font-size: 1.7em;
+  padding: 0.4em 0;
+  color: white;
+`;
+
+const Actions = styled.div`
+  display: flex;
+  flex-grow: 1;
+  justify-content: flex-end;
+  align-items: center;
+`;
 
 const Header = ({ title, actions }) => (
-  <div>
-    <h1>{title}</h1>
-    {actions && actions.map(component => component)}
-  </div>
+  <TopBar>
+    <Title>{title}</Title>
+    <Actions>{actions && actions.map(component => component)}</Actions>
+  </TopBar>
 );
 
 export default Header;
