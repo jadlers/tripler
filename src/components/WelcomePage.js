3b8fcf0ab4f769a60a0582579dce4e1289c92995
Jacob Adlers <jacob.adlers@gmail.com>
2018-08-12 21:42:19 +0200

    Show welcome text on material-ui card

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 import * as routes from '../constants/routes';
 import Header from './Header';
@@ -11,21 +15,30 @@ const LoginButton = (
   </Button>
 );
 
+const StyledCard = styled(Card)`
+  margin: 0.5em;
+`;
+
 const WelcomePage = () => {
   const headerActions = [LoginButton];
 
   return (
     <div>
       <Header title={'TripleR'} actions={headerActions} />
-      <p>
-        Welcome to TripleR which is short for reflect, record, and remember.
-      </p>
-      <p>
-        The idea is that you’ll sit down about three to five minutes each day to
-        reflect on how the day went. What was the highlight of the day? Did you
-        acomplish what you planned on doing yesterday?
-      </p>
-      <p>Get started by signing in!</p>
+
+      <StyledCard raised>
+        <CardContent>
+          <p>
+            Welcome to TripleR which is short for reflect, record, and remember.
+          </p>
+          <p>
+            The idea is that you’ll sit down about three to five minutes each
+            day to reflect on how the day went. What was the highlight of the
+            day? Did you acomplish what you planned on doing yesterday?
+          </p>
+          <p>Get started by signing in!</p>
+        </CardContent>
+      </StyledCard>
     </div>
   );
 };
